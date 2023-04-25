71f41036f34dc9e5376ece2c20cd731ef5de8ccf
espoupou <espoirativon@gmail.com>
2023-04-25 05:55:17 -0700

    betty

--- utils.c
+++ utils.c
@@ -47,14 +47,10 @@ int _puts(char *s)
 
 char *convert(unsigned long int num, int base, int lowercase)
 {
-	/*static char *rep;*/
 	int a;
 	static char buffer[50];
 	char *ptr;
 
-	/*rep = (lowercase)
-		? "0123456789abcdef"
-		: "0123456789ABCDEF";*/
 	ptr = &buffer[49];
 	*ptr = '\0';
 	do {
